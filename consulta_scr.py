f5624033a760af6ac35da0173afafa1b29d4c505
adriano.coelho <adriano.coelho@ulend.com.br>
2022-06-10 18:24:15 -0300

    updated df_parsed dataframe columns

--- consulta_scr.py
+++ consulta_scr.py
@@ -3,23 +3,86 @@ import json
 
 
 def main():
-    with open('/home/adriano/dev/projects/consulta_scr/source/teste2.csv', 'r', encoding='ISO-8859-1') as file:
-        sheet = pd.read_csv(file, header=0, sep=',')
+    with open('/home/adriano/dev/projects/consulta_scr/source/teste2.csv', 'r', encoding='utf-8') as file:
+        sheet = pd.read_csv(file)
         df = pd.DataFrame(sheet)
-        df.insert(len(df.columns)-1, 'message.listaDeResumoDasOperacoes', df.pop('message.listaDeResumoDasOperacoes'))
-        # ['message.listaDeResumoDasOperacoes']
+        # df.insert(len(df.columns)-1, 'message.listaDeResumoDasOperacoes', df.pop('message.listaDeResumoDasOperacoes'))
 
-        df_json = df.pop('message.listaDeResumoDasOperacoes')
-        # print(df_json)
-        df_teste = df_json[0]
+        # column_names_source = [
+        #     _id,
+        #     message.cnpjDaIFSolicitante,
+        #     message.codigoDoCliente,
+        #     message.dataBaseConsultada,
+        #     message.listaDeResumoDasOperacoes
+        # ]
 
-        json1 = json.loads(df_teste)
-        print(json1)
-        print(type(json1))
+        column_names_parsed = [
+            '_id',
+            'cnpjCliente',
+            'dataConsulta',
+            'dataBase',
+            'codigoModalidade',
+            'v20',
+            'v40',
+            'v60',
+            'v80',
+            'v110',
+            'v120',
+            'v130',
+            'v140',
+            'v150',
+            'v160',
+            'v165',
+            'v170',
+            'v175',
+            'v180',
+            'v190',
+            'v199',
+            'v205',
+            'v210',
+            'v220',
+            'v230',
+            'v240',
+            'v245',
+            'v250',
+            'v255',
+            'v260',
+            'v270',
+            'v280',
+            'v290',
+            'v310',
+            'v320',
+            'v330'
+        ]
+        df_parsed = pd.DataFrame(columns=column_names_parsed)
+        df_parsed['_id'] = df['_id']
 
+        # id_parsed = df['_id']
+        # print(id_parsed)
+
+        for row, item in df_parsed['_id'].iteritems():
+            print(row, type(row))
+            print(item, type(item))
+
+        #     # extrair a coluna com o json e atribuir a outro dataframe
+        #     df_json = pd.DataFrame.pop(row['message.listaDeResumoDasOperacoes'])
+        #     df_teste = df_json
+        #
+        #     # coluna do json passando os dados pra list
+        #     json1 = json.loads(df_teste)
+        #     print('num de objetos do json: ', len(json1))
+        #     # print(json1)
+        #     # print(type(json1))  # list
+        #
         # # routine trough all the rows
-        # for row in df_json:
-        #     print(row)
+        # for row in json1:
+        #     print(len(row))
+        #     # print(row)
+        #
+        # df_parsed['cnpjCliente'] = df['message.cnpjDaIFSolicitante']
+        # df_parsed['dataConsulta'] = df['createdAt']
+        # df_parsed['dataBase'] = df['message.dataBaseConsultada']
+        # print(df_parsed)
 
         # df2 = pd.DataFrame(pd.json_normalize(df['message.listaDeResumoDasOperacoes']))
         # print(df2)
