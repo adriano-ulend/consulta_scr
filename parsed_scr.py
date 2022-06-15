60c318f389401fd8e768dca49db6fed77acef3b1
adriano.coelho <adriano.coelho@ulend.com.br>
2022-06-15 17:19:25 -0300

    extracted final dataframe version1

--- parsed_scr.py
+++ parsed_scr.py
@@ -4,16 +4,24 @@ import pandas as pd
 
 
 def main():
+    """
+    Read a .CSV file and extract all data from specific columns into a DataFrame and parsing data from column
+    'message.listaDeResumoDasOperacoes' as a json and creating another Dataframe through iterable method
+
+    :return: Two Dataframe joined into one showing a table with all data (codigoVencimento e valorVencimento for each
+    'modalidade' for each CNPJ from the file source
+    """
+
     with open('/home/adriano/dev/projects/consulta_scr/source/teste2.csv', 'r', encoding='utf-8') as file:
+        # teste.csv = file original / teste2.csv = test file
         sheet = csv.reader(file, delimiter=',')
 
         # column_names_source = [
-        #     _id, - 0
-        #     createdAt, - 1
-        #     message.cnpjDaIFSolicitante, - 3
-        #     message.codigoDoCliente, - 4
-        #     message.dataBaseConsultada, - 7
-        #     message.listaDeResumoDasOperacoes - 10
+        #     '_id', 0
+        #     'createdAt', 1
+        #     'message.codigoDoCliente', 4
+        #     'message.dataBaseConsultada', 7
+        #     'dict_mod_CodigoValor'
         # ]
 
         column_names_parsed = [
@@ -56,30 +64,34 @@ def main():
         for e, line in enumerate(sheet):
             #    line 0 = headers
             if e == 0:
+                # header = column_names_source
                 header = [line[0], line[1], line[4], line[7], 'dict_mod_CodigoValor']
                 data_header.append(header)
             else:
-                json_line = json.loads(line[10])
-                # print('num de objetos do json: ', len(json_line))
-                # print(json_line)
-                # print(type(json_line))  # list
+                if line[10] == "":
+                    print(f'{line[0]} on line {e} does not have any SCR data on the record')
+                else:
+                    json_line = json.loads(line[10])
+                    # print('num de objetos do json: ', len(json_line))
+                    # print(json_line)
+                    # print(type(json_line))  # list
 
-                for item in json_line:
-                    # print(f'item: {item}')
-                    # print(f'type: {type(item)}')
-                    # list_temp = []
-                    modalidade = item['modalidade']
+                    for item in json_line:
+                        # print(f'item: {item}')
+                        # print(f'type: {type(item)}')
+                        # list_temp = []
+                        modalidade = item['modalidade']
 
-                    vencimento_temp = {}
-                    mod_venc_temp = {}
-                    for data_venc in item['listaDeVencimentos']:
-                        vencimento_temp[f"{data_venc['codigoVencimento']}"] = f"{data_venc['valorVencimento']}"
-                        # print(vencimento_temp)
+                        vencimento_temp = {}
+                        mod_venc_temp = {}
+                        for data_venc in item['listaDeVencimentos']:
+                            vencimento_temp[f"{data_venc['codigoVencimento']}"] = f"{data_venc['valorVencimento']}"
+                            # print(vencimento_temp)
 
-                        mod_venc_temp[f"{modalidade}"] = vencimento_temp
+                            mod_venc_temp[f"{modalidade}"] = vencimento_temp
 
-                    selected_params = [line[0], line[1], line[4], line[7], mod_venc_temp]
-                    data_list.append(selected_params)
+                        selected_params = [line[0], line[1], line[4], line[7], mod_venc_temp]
+                        data_list.append(selected_params)
 
         table = pd.DataFrame(data_list, columns=data_header)
         df_cod_val = table.pop('dict_mod_CodigoValor')
@@ -97,8 +109,15 @@ def main():
                         # colum_index = column_names_parsed.index(venc_cod)
                         df_vencimentos.loc[df_vencimentos.shape[0] - 1, f"{venc_cod}"] = vencim_valor
 
-        print(df_vencimentos)
+        df_vencimentos = df_vencimentos.fillna(0)
+
+        table_parsed = pd.merge(table, df_vencimentos, how='left', left_index=True, right_index=True)
+
+        # makes a copy of parsed dataframe to another excel file
+        table_parsed.to_csv('/home/adriano/dev/projects/consulta_scr/source/parsed_scr.csv', index=False)
+
+        return "consulta_scr gerado"
 
 
 if __name__ == '__main__':
-    print(main())
+    main()
