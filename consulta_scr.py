14077223a01aa0e6bd633423fbe103a4cc9e4739
adriano.coelho <adriano.coelho@ulend.com.br>
2022-06-23 17:33:12 -0300

    extract uuid from clients to access scr consult history

--- consulta_scr.py
+++ consulta_scr.py
@@ -57,6 +57,12 @@ def check_scr(token, cnpj, data_op):
 
 
 def scr_hist(token, cnpj):
+    """
+
+    :param token:
+    :param cnpj:
+    :return:
+    """
 
     endpoint_company_data = f"https://api.ulend.com.br/company"
 
@@ -88,7 +94,11 @@ def scr_hist(token, cnpj):
     return scr_data
 
 
-def extract():
+def extract() -> list:
+    """
+    Open a csv file to extract cnpj data from all clients on system database
+    :return: list of all cnpj/uuid to be checked on credit-analysis
+    """
     with open(f'{current_path}'+'/source/teste3.csv', 'r', encoding='utf-8') as file:
         sheet = pd.read_csv(file, encoding='utf-8')
         client_source = pd.DataFrame(sheet)
@@ -96,17 +106,49 @@ def extract():
     return client_source
 
 
+def all_clients(token_prod):
+
+    all_clients_data = []
+    all_clients_temp = []
+
+    endpoint_all_clients = f"https://api.ulend.com.br/company/-/loan"
+
+    response_all_clients = requests.get(
+        endpoint_all_clients,
+        headers={
+            "Accept-version": 'v1+',
+            "Authorization": f'{token_prod}'
+        },
+        params={"page": "1", "limit": "500"}
+    )
+    response_all_clients_json = response_all_clients.json()
+
+    for client in response_all_clients_json['loan']:
+        all_clients_temp.append(client['company_cnpj'])
+        all_clients_temp.append(client['uuid'])
+        all_clients_temp.append(client['company_name'])
+        all_clients_data.append(all_clients_temp)
+        all_clients_temp = []
+
+    return all_clients_data
+
+
 if __name__ == '__main__':
     print("SCR CONSULTING\n")
 
+
     signin_token = token()
     print(signin_token)
+
+    all_clients(signin_token)
+    print('ok')
 
     # t = extract()
     # print(t)
 
     cnpj_teste = '06914893000167'   # comercio de pneus anadia ltda
     data_teste = '2021-08'  # 2022-04 a 2021-05
+    # id_teste = '62a7a27b4f7b441597ae3fd8'   # id no sistema do cliente
 
     print(check_scr(signin_token, cnpj_teste, data_teste))
 
@@ -115,8 +157,10 @@ if __name__ == '__main__':
     print("\nend\n")
 
 # CHECKLIST:
-# 1. verificar como retornar o uuid do cliente para buscar o historico scr no mongo
-# 2. testar condicional caso nao tenha historico de consulta scr no mongo
+# 1. verificar como retornar o uuid do cliente para buscar o historico scr no mongo - all_list >> search
+#     1.1. response de company/-/loan gera uma lista com info de clients (atende a demanda a ser analisada?)
+# 2. testar condicional caso nao tenha historico de consulta scr na analise de credito
+#     2.1. se nao tiver faz uma consulta scr dos ultimos 12 meses
 # 3. validar response data da request para realizar uma nova consulta scr
 # 4. varificar possiveis cenarios de erro (realizar teste com source file_test pelo extract)
 # 5. validar output para ser input do parsed_scr
